f11aca2e9436d0df1eef239185cf6bf863159655
Daniel Segura <danielseguraf@gmail.com>
2022-02-03 00:02:17 -0500

    package.json updated

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 9000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Peruvian Food API 🧄🧅🥔🍋');
 });
 
-app.listen(1234, console.log(`Server running on Port 1234 🔥`));
+app.listen(PORT, console.log(`Server running on Port 1234 🔥`));
